33651c50d270b465263f02158633b7c3e276f245
Wesley1982 <56592331+Wesley1982@users.noreply.github.com>
2021-03-15 10:23:38 +0100

    Update intro.py

--- intro.py
+++ intro.py
@@ -10,15 +10,13 @@ with open ('processor_table.csv', 'w') as f:
         res = requests.get(f'https://www.intel.co.uk/content/www/uk/en/products/processors/xeon/view-all.html?page={i}')
         html = res.text
         soup = BeautifulSoup(html, 'html.parser')
-        for n in soup.find_all('div', {'class': 'content-wrap valign-btm'}):
-            k = n.find('span', {'itemprop':'name'})
-            print(k.string)
-            processor = re.search(r'\w{0,1}\-?\d{4}\w{0,2}\s?[v]?\d?', k.string).group()
+        for div in soup.find_all('div', {'class': 'content-wrap valign-btm'}):
+            span = div.find('span', {'itemprop':'name'})
+            processor = re.search(r'\w{0,1}\-?\d{4}\w{0,2}\s?[v]?\d?', span.string).group()
             print('----------------------------------')
-            for l in n.find_all('li'):
-                if 'Cores' in l.string:
-                    cores = l.string
-            print(processor, cores)
+            for li in div.find_all('li'):
+                if 'Cores' in li.string:
+                    cores = li.string
             f.write(f'{processor}, {cores}')
             f.write('\n')
             
